5afc90545c1b69f628565aaae496ac12e7b55b11
Rareș Lițescu <105374710+Railysse@users.noreply.github.com>
2022-08-27 11:35:10 +0300

    Update sql code.sql

--- sql code.sql
+++ sql code.sql
@@ -1,3 +1,110 @@
+# creating the tables
+CREATE TABLE accident (
+        accident_index VARCHAR(30),
+        accident_year INT,
+        accident_reference VARCHAR(30),
+        location_easting_osgr DECIMAL(16, 8),    
+        location_northing_osgr DECIMAL(16, 8),   
+        longitude DECIMAL(16, 8),
+        latitude DECIMAL(16, 8),
+        police_force INT,
+        accident_severity INT,
+        number_of_vehicles INT,
+        number_of_casualties INT,
+        date VARCHAR(30),
+        day_of_week INT,
+        time VARCHAR(30),
+        local_authority_district INT,
+        local_authority_ons_district VARCHAR(30),
+        local_authority_highway VARCHAR(30),
+        first_road_class INT,
+        first_road_number INT,
+        road_type INT,
+        speed_limit INT,
+        junction_detail INT,
+        junction_control INT,
+        second_road_class INT,
+        second_road_number INT,
+        pedestrian_crossing_human_control INT,
+        pedestrian_crossing_physical_facilities INT,
+        light_conditions INT,
+        weather_conditions INT,
+        road_surface_conditions INT,
+        special_conditions_at_site INT,
+        carriageway_hazards INT,
+        urban_or_rural_area INT,
+        did_police_officer_attend_scene_of_accident INT,
+        trunk_road_flag INT,
+        lsoa_of_accident_location VARCHAR(30)
+);
+CREATE TABLE vehicle (
+        accident_index VARCHAR(30),
+        accident_year INT,
+        accident_reference VARCHAR(30),
+        vehicle_reference INT,
+        vehicle_type INT,
+        towing_and_articulation INT,
+        vehicle_manoeuvre INT,
+        vehicle_direction_from INT,
+        vehicle_direction_to INT,
+        vehicle_location_restricted_lane INT,
+        junction_location INT,
+        skidding_and_overturning INT,
+        hit_object_in_carriageway INT,
+        vehicle_leaving_carriageway INT,
+        hit_object_off_carriageway INT,
+        first_point_of_impact INT,
+        vehicle_left_hand_drive INT,
+        journey_purpose_of_driver INT,
+        sex_of_driver INT,
+        age_of_driver INT,
+        age_band_of_driver INT,
+        engine_capacity_cc INT,
+        propulsion_code INT,
+        age_of_vehicle INT,
+        generic_make_model VARCHAR(30),
+        driver_imd_decile INT,
+        driver_home_area_type INT
+);
+CREATE TABLE casualty (
+        accident_index VARCHAR(30),
+        accident_year INT,
+        accident_reference VARCHAR(30),
+        vehicle_reference INT,
+        casualty_reference INT,
+        casualty_class INT,
+        sex_of_casualty INT,
+        age_of_casualty INT,
+        age_band_of_casualty INT,
+        casualty_severity INT,
+        pedestrian_location INT,
+        pedestrian_movement INT,
+        car_passenger INT,
+        bus_or_coach_passenger INT,
+        pedestrian_road_maintenance_worker INT,
+        casualty_type INT,
+        casualty_home_area_type INT,
+        casualty_imd_decile INT
+); 
+
+
+# loading the data
+LOAD DATA INFILE 'E:/road_safety/accident_prepped.csv'
+INTO TABLE accident
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS;		
+
+LOAD DATA INFILE 'E:/road_safety/vehicle_prepped.csv'
+INTO TABLE vehicle	
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS;
+	
+LOAD DATA INFILE 'E:/road_safety/casualty_prepped.csv'
+INTO TABLE casualty
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS;
+
+
 # population distribution by age
 SELECT age_of_driver, COUNT(*) AS count
 FROM vehicle
